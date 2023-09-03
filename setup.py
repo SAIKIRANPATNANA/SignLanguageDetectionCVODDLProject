d1087e3159bfd9ef81878e9a2124183629e9e1b7
SAIKIRANPATNANA <saikiranpatnana5143@gmail.com>
2023-09-03 10:41:20 +0000

    signLanguageDetection

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguageps',
+    version= '0.0.0',
+    author= 'SAIKIRANPATNANA',
+    author_email= 'saikiranpatnana5143@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
